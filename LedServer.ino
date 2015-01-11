0d46edcc4ce39e156b5251b304b58e4037b4bd19
Chase Roberts <keeper6928@gmail.com>
2015-01-10 18:26:05 -0600

    Arduino Code

--- LedServer.ino
+++ LedServer.ino
@@ -114,7 +114,6 @@ void loop()
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the respons
-          client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
           client.println("Signal Recieved");
